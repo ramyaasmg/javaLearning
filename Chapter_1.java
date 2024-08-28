99de125a1d48db9f85487e51d34f9dd01831b142
Ramya <142328512+ramyaasmg@users.noreply.github.com>
2024-08-28 13:53:28 +0530

    Add files via upload

---
+++ Chapter_1.java
@@ -0,0 +1,9 @@
+package com.company; // Groups of classes
+public class Chapter_1 { //Entrypoint into the application
+    public static void main(String[]args){ //this is the main method
+        System.out.println("hello Ramya");
+        
+    }
+}
+//for classes,we use Pascal Convention naming
+// for functions and variables, we use camelCaseConvention
