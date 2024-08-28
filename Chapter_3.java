ef205b8ee10e382839177a802c28eaf6d920f85d
Ramya <142328512+ramyaasmg@users.noreply.github.com>
2024-08-28 15:11:50 +0530

    Add files via upload

---
+++ Chapter_3.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Chapter_3 {
+    public static void main (String[]args){
+// Literals in Java
+//Constants value that can assigned ot the variable is called literals
+        byte no=12;
+        short no1=122;
+        int no3=56567;
+        long no4=76876L;
+        char cr='R';
+        float f=88.24f;
+        double d=98.79889;
+        boolean b=true;
+        String str="Shiro";
+        System.out.println(str);
+
+
+    }
+}
