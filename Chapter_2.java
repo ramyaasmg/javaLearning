f8b18b704c8f680168dad7ceabcda26b28410000
Ramya <142328512+ramyaasmg@users.noreply.github.com>
2024-08-28 14:45:58 +0530

    Add files via upload

---
+++ Chapter_2.java
@@ -0,0 +1,20 @@
+package com.company;
+public class Chapter_2 {
+    public static void main(String[]args){
+        byte a;// byte store values of 1byte means -128 to 127
+        short b;// short store values of 1 byte means -32,768 to 32767
+        int c;// int store values of 2byte -2,147,483,648 to 2,147,483,647
+        long d;// this we store the value of 4byte
+        float e;//means store 8bytes values
+        double f;// it stores values of 8 bytes
+        char g;// it stores 2byte
+        boolean h;// it depends on JVM
+        System.out.print("sum of this numbers is:");
+
+//Write a Java program to add three numbers
+        int num1=5;
+        int num2=10;
+        int sum=num1+num2;
+        System.out.println(sum);
+    }
+}
